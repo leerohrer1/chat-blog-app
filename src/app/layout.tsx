f5bc77de8b1312e92cc568d6080d3102c5c75eeb
leerohrer1 <leerohrer1@gmail.com>
2025-05-05 20:22:35 -0500

    changed browser tab titles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,8 +10,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: `Next.js Blog Example with ${CMS_NAME}`,
-  description: `A statically generated blog example using Next.js and ${CMS_NAME}.`,
+  title: `Lee Rohrer`,
+  description: `Building software with curiosity, collaboration, and care.`,
   openGraph: {
     images: [HOME_OG_IMAGE_URL],
   },
