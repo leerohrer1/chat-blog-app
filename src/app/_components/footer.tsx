f7d47bf63aa626b88be0f127c10bdfdf511b8f60
leerohrer1 <leerohrer1@gmail.com>
2025-05-05 19:18:04 -0500

    change to header and footer

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -1,26 +1,35 @@
 import Container from "@/app/_components/container";
-import { EXAMPLE_PATH } from "@/lib/constants";
 
 export function Footer() {
   return (
     <footer className="bg-neutral-50 border-t border-neutral-200 dark:bg-slate-800">
       <Container>
         <div className="py-28 flex flex-col lg:flex-row items-center">
-          <h3 className="text-4xl lg:text-[2.5rem] font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            Statically Generated with Next.js.
-          </h3>
-          <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
+          <div className="text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
+            <h3 className="text-4xl lg:text-[2.5rem] font-bold tracking-tighter leading-tight">
+              Contact Lee
+            </h3>
             <a
-              href="https://nextjs.org/docs/app/building-your-application/routing/layouts-and-templates"
-              className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
+              href="https://www.linkedin.com/in/lee-rohrer-14322486/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-2xl font-bold tracking-tighter mt-3 block text-neutral-900 dark:text-white hover:underline"
             >
-              Read Documentation
+              LinkedIn
             </a>
             <a
-              href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
-              className="mx-3 font-bold hover:underline"
+              href="mailto:leerohrer1@gmail.com"
+              rel="noopener noreferrer"
+              className="text-2xl font-bold tracking-tighter mt-3 block text-neutral-900 dark:text-white hover:underline"
             >
-              View on GitHub
+              Email
+            </a>
+            <a
+              href="tel:636-233-0687"
+              rel="noopener noreferrer"
+              className="text-2xl font-bold tracking-tighter mt-3 block text-neutral-900 dark:text-white hover:underline"
+            >
+              Call
             </a>
           </div>
         </div>
