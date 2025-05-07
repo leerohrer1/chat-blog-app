716611df39bc47aa653ed98f3b65c14f2f0d8421
leerohrer1 <leerohrer1@gmail.com>
2025-05-06 20:37:10 -0500

    chat section added with some styling, without state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Container from "@/app/_components/container";
 import { Intro } from "@/app/_components/intro";
 import { MoreStories } from "@/app/_components/more-stories";
 import { getAllPosts } from "@/lib/api";
-
+import { ChatSection } from "@/app/_components/chat";
 export default function Index() {
   const allPosts = getAllPosts();
 
@@ -10,6 +10,7 @@ export default function Index() {
     <main>
       <Container>
         <Intro />
+        <ChatSection />
         <MoreStories posts={allPosts} />
       </Container>
     </main>
